809e3eaaf3acd947fdb1fc397f283dd6f4fe59ce
Alessandro <asovitorio@gmail.com>
2020-11-26 01:53:10 -0300

    estudo de variaveis

---
+++ variaveis.js
@@ -0,0 +1,30 @@
+let a = 5
+let b = 2
+
+let parOuImpar = 11
+const resultado = parOuImpar % 2 == 0 ? 'A variavel é par' : 'a vairavel é Impar';
+
+if (parOuImpar % 2 == 0) {
+    console.log('A variavel é par')
+} else {
+    if (parOuImpar > 10) {
+        console.log('A variavel é Impar e > que dez')
+    } else {
+        console.log('A variavel é Impar e < que dez')
+    }
+}
+console.log(resultado)
+
+
+// tipos de dados das variaveis
+
+
+let numero = 4
+let texto = 'texto'
+let boolean = false
+let array = []
+let objeto = {}
+let funcao = () => {}
+let n = null
+
+const carros = ['monza', 'amarock', 'ecosport']
